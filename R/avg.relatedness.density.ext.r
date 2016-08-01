3eb6eb9fa5ead38831f6c87984adb718287c454b
PIERRE-ALEXANDRE BALLAND <p.balland@uu.nl>
2016-07-31 18:48:53 -0700

    ok

--- R/avg.relatedness.density.ext.r
+++ R/avg.relatedness.density.ext.r
@@ -1,6 +1,6 @@
 #' Compute the average relatedness density of regions to industries that are not part of the regional portfolio from regions - industries matrices and industries - industries matrices
 #'
-#' This function computes the average relatedness density of regions to industries that are not part of the regional portfolio from regions - industries (incidence) matrices and industries - industries (adjacency) matrices. This is the technological flexibility indicator proposed by Balland et al. (2015). 
+#' This function computes the average relatedness density of regions to industries that are not part of the regional portfolio from regions - industries (incidence) matrices and industries - industries (adjacency) matrices. This is the technological flexibility indicator proposed by Balland et al. (2015).
 #' @param mat An incidence matrix with regions in rows and industries in columns
 #' @param relatedness An adjacency industry - industry matrix indicating the degree of relatedness between industries
 #' @keywords relatedness density co-occurences relatedness normalized co-occurences
@@ -8,16 +8,16 @@
 #' @examples
 #' ## generate a region - industry matrix in which cells represent the presence/absence of a RCA
 #' set.seed(31)
-#' mat <- matrix(sample(0:1,20,replace=T), ncol = 4) 
+#' mat <- matrix(sample(0:1,20,replace=T), ncol = 4)
 #' rownames(mat) <- c ("R1", "R2", "R3", "R4", "R5")
 #' colnames(mat) <- c ("I1", "I2", "I3", "I4")
-#' 
+#'
 #' ## generate an industry - industry matrix in which cells indicate if two industries are related (1) or not (0)
-#' relatedness <- matrix(sample(0:1,16,replace=T), ncol = 4) 
+#' relatedness <- matrix(sample(0:1,16,replace=T), ncol = 4)
 #' relatedness[lower.tri(relatedness, diag = TRUE)] <- t(relatedness)[lower.tri(t(relatedness), diag = TRUE)]
 #' rownames(relatedness) <- c ("I1", "I2", "I3", "I4")
 #' colnames(relatedness) <- c ("I1", "I2", "I3", "I4")
-#' 
+#'
 #' ## run the function
 #' avg.relatedness.density.ext (mat, relatedness)
 #' @author Pierre-Alexandre Balland \email{p.balland@uu.nl}
