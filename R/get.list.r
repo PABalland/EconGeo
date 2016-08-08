fee2f30b25b9b0f52a68e25e7e265f5e4cbe1672
PIERRE-ALEXANDRE BALLAND <p.balland@uu.nl>
2016-08-08 15:15:23 -0700

    ok

--- R/get.list.r
+++ R/get.list.r
@@ -1,27 +1,31 @@
-#' Create regular data frames from regions - industries matrices 
+#' Create regular data frames from regions - industries matrices
 #'
 #' This function creates regular data frames with three columns (regions, industries, count) from (incidence) matrices (wide to long format) using the reshape2 package
 #' @param mat An incidence matrix with regions in rows and industries in columns (or the other way around)
 #' @keywords adjacency matrix, edge lists, reshape package, economics, geography, economic geography
 #' @export
 #' @examples
-#' ## generate a region - industry data frame
+#' ## generate a region - industry matrix
 #' set.seed(31)
-#' region <- c("R1", "R1", "R1", "R1", "R2", "R2", "R3", "R4", "R5")
-#' industry <- c("I1", "I2", "I3", "I4", "I1", "I2", "I1", "I1", "I3")
-#' data <- data.frame (region, industry)
-#' data$count <- 1
-#' 
+#' mat <- matrix(sample(0:100,20,replace=T), ncol = 4)
+#' rownames(mat) <- c ("R1", "R2", "R3", "R4", "R5")
+#' colnames(mat) <- c ("I1", "I2", "I3", "I4")
+#'
 #' ## run the function
 #' get.list (mat)
 #' @author Pierre-Alexandre Balland \email{p.balland@uu.nl}
 #' @seealso \code{\link{get.matrix}}
 
+
 get.list <- function(mat) {
-  library (reshape2)
-  list <- melt(mat)
+
+  library (Matrix)
+  list = Matrix (mat, sparse = T)
+  list = summary (list)
+  list = as.data.frame (as.matrix(list))
   colnames (list) <- c ("Region", "Industry", "Count")
   return (list)
+
 }
 
 
