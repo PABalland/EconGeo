086331865b5945302b5bf6c6ca4914a213d632e7
PIERRE-ALEXANDRE BALLAND <p.balland@uu.nl>
2016-08-18 17:27:09 -0500

    include coordinates in Lorenz/Hoover curves

--- R/Hoover.curve.r
+++ R/Hoover.curve.r
@@ -3,6 +3,7 @@
 #' This function plots a Hoover curve from regions - industries matrices.
 #' @param mat An incidence matrix with regions in rows and industries in columns. The input can also be a vector of industrial regional count (a matrix with n regions in rows and a single column).
 #' @param pop A vector of population regional count
+#' @param plot Logical; shall the curve be automatically plotted? Defaults to TRUE. If set to TRUE, the function will return x y coordinates that you can latter use to plot and customize the curve.
 #' @param pdf Logical; shall a pdf be saved to your current working directory? Defaults to FALSE. If set to TRUE, a pdf with all Hoover curves will be compiled and saved to your current working directory.
 #' @keywords concentration inequality
 #' @export
@@ -14,6 +15,7 @@
 #' ## run the function (30% of the population produces 50% of the industrial output)
 #' Hoover.curve (ind, pop)
 #' Hoover.curve (ind, pop, pdf = TRUE)
+#' Hoover.curve (ind, pop, plot = F)
 #'
 #' ## generate a region - industry matrix
 #' mat = matrix (
@@ -28,26 +30,32 @@
 #' ## run the function
 #' Hoover.curve (mat, pop)
 #' Hoover.curve (mat, pop, pdf = TRUE)
+#' Hoover.curve (mat, pop, plot = FALSE)
 #'
 #' ## run the function by aggregating all industries
 #' Hoover.curve (rowSums(mat), pop)
 #' Hoover.curve (rowSums(mat), pop, pdf = TRUE)
+#' Hoover.curve (rowSums(mat), pop, plot = FALSE)
 #'
 #' ## run the function for industry #1 only
 #' Hoover.curve (mat[,1], pop)
 #' Hoover.curve (mat[,1], pop, pdf = TRUE)
+#' Hoover.curve (mat[,1], pop, plot = FALSE)
 #'
 #' ## run the function for industry #2 only (perfectly proportional to population)
 #' Hoover.curve (mat[,2], pop)
 #' Hoover.curve (mat[,2], pop, pdf = TRUE)
+#' Hoover.curve (mat[,2], pop, plot = FALSE)
 #'
 #' ## run the function for industry #3 only (30% of the pop. produces 100% of the output)
 #' Hoover.curve (mat[,3], pop)
 #' Hoover.curve (mat[,3], pop, pdf = TRUE)
+#' Hoover.curve (mat[,3], pop, plot = FALSE)
 #'
 #' ## run the function for industry #4 only (55% of the pop. produces 100% of the output)
 #' Hoover.curve (mat[,4], pop)
 #' Hoover.curve (mat[,4], pop, pdf = TRUE)
+#' Hoover.curve (mat[,4], pop, plot = FALSE)
 #'
 #' Compare the distribution of the #industries
 #' par(mfrow=c(2,2))
@@ -60,7 +68,29 @@
 #' @seealso \code{\link{Hoover.Gini}}, \code{\link{locational.Gini}}, \code{\link{locational.Gini.curve}}, \code{\link{Lorenz.curve}}, \code{\link{Gini}}
 #' @references Hoover, E.M. (1936) The Measurement of Industrial Localization, \emph{The Review of Economics and Statistics} \strong{18} (1): 162-171
 
-Hoover.curve <- function(mat, pop, pdf = FALSE) {
+Hoover.curve <- function(mat, pop, plot = TRUE, pdf = FALSE) {
+
+  if (!plot) {
+
+    mat = as.matrix (mat)
+    ind <- c(0, mat[,1])
+    pop <- c(0, pop)
+    c = data.frame (ind, pop)
+    c = c[complete.cases(c),]
+    ind = c$ind
+    pop = c$pop
+    o = ind/pop
+    o[is.na(o)] = 0
+    oind <- order(o)
+    ind <- ind[oind]
+    pop <- pop[oind]
+    cind <- cumsum(ind)/max(cumsum(ind))
+    cpop <- cumsum(pop)/max(cumsum(pop))
+    return (list(cum.reg = cpop, cum.out = cind))
+
+  }
+
+  if (plot) {
 
   mat = as.matrix (mat)
 
@@ -104,8 +134,11 @@ Hoover.curve <- function(mat, pop, pdf = FALSE) {
     dev.off()
     print ("Hoover.curve.pdf has been saved to your current working directory")
 
-  }
-
 }
+
+  }
+}
+
+
 
 
--- R/Lorenz.curve.r
+++ R/Lorenz.curve.r
@@ -3,6 +3,7 @@
 #' This function plots a Lorenz curve from regional industrial counts. This curve gives an indication of the unequal distribution of an industry accross regions.
 #' @param mat An incidence matrix with regions in rows and industries in columns. The input can also be a vector of industrial regional count (a matrix with n regions in rows and a single column).
 #' @param pdf Logical; shall a pdf be saved to your current working directory? Defaults to FALSE. If set to TRUE, a pdf with all Lorenz curves will be compiled and saved to your current working directory.
+#' @param plot Logical; shall the curve be automatically plotted? Defaults to TRUE. If set to TRUE, the function will return x y coordinates that you can latter use to plot and customize the curve.
 #' @keywords concentration inequality
 #' @export
 #' @examples
@@ -12,6 +13,7 @@
 #' ## run the function
 #' Lorenz.curve (ind)
 #' Lorenz.curve (ind, pdf = TRUE)
+#' Lorenz.curve (ind, plot = FALSE)
 #'
 #' ## generate a region - industry matrix
 #' mat = matrix (
@@ -26,26 +28,32 @@
 #' ## run the function
 #' Lorenz.curve (mat)
 #' Lorenz.curve (mat, pdf = TRUE)
+#' Lorenz.curve (mat, plot = FALSE)
 #'
 #' ## run the function by aggregating all industries
 #' Lorenz.curve (rowSums(mat))
-#' Lorenz.curve (rowSums(mat))
+#' Lorenz.curve (rowSums(mat), pdf = TRUE)
+#' Lorenz.curve (rowSums(mat), plot = FALSE)
 #'
 #' ## run the function for industry #1 only (perfect equality)
 #' Lorenz.curve (mat[,1])
 #' Lorenz.curve (mat[,1], pdf = TRUE)
+#' Lorenz.curve (mat[,1], plot = FALSE)
 #'
 #' ## run the function for industry #2 only (perfect equality)
 #' Lorenz.curve (mat[,2])
 #' Lorenz.curve (mat[,2], pdf = TRUE)
+#' Lorenz.curve (mat[,2], plot = FALSE)
 #'
 #' ## run the function for industry #3 only (perfect unequality)
 #' Lorenz.curve (mat[,3])
 #' Lorenz.curve (mat[,3], pdf = TRUE)
+#' Lorenz.curve (mat[,3], plot = FALSE)
 #'
 #' ## run the function for industry #4 only (top 40% produces 100% of the output)
 #' Lorenz.curve (mat[,4])
 #' Lorenz.curve (mat[,4], pdf = TRUE)
+#' Lorenz.curve (mat[,4], plot = FALSE)
 #'
 #' Compare the distribution of the #industries
 #' par(mfrow=c(2,2))
@@ -58,7 +66,29 @@
 #' @seealso \code{\link{Hoover.Gini}}, \code{\link{locational.Gini}}, \code{\link{locational.Gini.curve}}, \code{\link{Hoover.curve}}, \code{\link{Gini}}
 #' @references Lorenz, M. O. (1905) Methods of measuring the concentration of wealth, \emph{Publications of the American Statistical Association} \strong{9}: 209–219
 
-Lorenz.curve <- function(mat, pdf = FALSE) {
+Lorenz.curve <- function(mat, pdf = FALSE, plot = TRUE) {
+
+  if (!plot) {
+
+    mat = as.matrix (mat)
+
+    x <- mat[,1]
+    x = x[complete.cases (x)]
+    weights = rep(1, length = length(x))
+    ox <- order(x)
+    x <- x[ox]
+    weights <- weights[ox]/sum(weights)
+    p <- cumsum(weights)
+    nu <- cumsum(weights * x)
+    n <- length(nu)
+    nu <- nu/nu[n]
+    p <- c(0,p)
+    nu <- c(0,nu)
+    return (list(cum.reg = p, cum.out = nu))
+
+  }
+
+  if (plot) {
 
   mat = as.matrix (mat)
 
@@ -105,4 +135,5 @@ Lorenz.curve <- function(mat, pdf = FALSE) {
 
 }
 
+}
 
