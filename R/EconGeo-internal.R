f25c815a1a99ef60da20396b5ed2b729c5f054f9
Pierre-Alexandre Balland <p.balland@uu.nl>
2018-06-13 10:47:59 -0400

    startup msg
    change startup msg

--- R/EconGeo-internal.R
+++ R/EconGeo-internal.R
@@ -1,6 +1,6 @@
 .onAttach <-
 function(libname, pkgname) {
   packageStartupMessage("\nPlease cite EconGeo in publications as: \n")
-  packageStartupMessage(" Balland, P.A. (2017) EconGeo: Computing Key Indicators of the Spatial Distribution of Economic Activities, R package version 1.3: https://github.com/PABalland/EconGeo \n")
+  packageStartupMessage(" Balland, P.A. (2017) Economic Geography in R: Introduction to the EconGeo Package, Papers in Evolutionary Economic Geography, 17 (09): 1-75 \n")
 }
 
